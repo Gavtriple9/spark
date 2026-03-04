843a35b923f37786f108005992d44694450bff42
Gavtriple9 <29155514+Gavtriple9@users.noreply.github.com>
2026-03-03 23:48:23 -0500

    Project init

---
+++ examples/demo.rs
@@ -0,0 +1,13 @@
+// use anyhow::Result;
+
+// fn main() -> Result<()> {
+//     println!("Hello, world!");
+//     let spark = spark::SparkEngine::default();
+//     spark.add_rigid_body(spark::RigidBody::new_box(
+//         spark::Vector3::new(0.0, 10.0, 0.0),
+//         spark::Vector3::new(1.0, 1.0, 1.0),
+//         1.0,
+//     ));
+//     spark.simulate_step(0.016);
+//     Ok(())
+// }
